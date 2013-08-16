94f98feee34e7bdb28f3c0794dc6f6dcc9b976f8
Thomas Lotze <tl@gocept.com>
2013-08-16 15:16:36 +0200

    shortened code for fixture creation in tests

--- src/gocept/pytestlayer/tests/fixture/keep_layer_across_test_classes/conftest.py
+++ src/gocept/pytestlayer/tests/fixture/keep_layer_across_test_classes/conftest.py
@@ -3,6 +3,4 @@ from gocept.pytestlayer.fixture import create
 
 
 pytest_plugins = ('zopelayer', 'capturelog')
-globals().update(create(FooLayer))
-globals().update(create(BarLayer))
-globals().update(create(FooBarLayer))
+globals().update(create(FooLayer, BarLayer, FooBarLayer))
--- src/gocept/pytestlayer/tests/fixture/two_dependent_layers/conftest.py
+++ src/gocept/pytestlayer/tests/fixture/two_dependent_layers/conftest.py
@@ -3,5 +3,4 @@ from gocept.pytestlayer.fixture import create
 
 
 pytest_plugins = ('zopelayer', 'capturelog')
-globals().update(create(FooLayer))
-globals().update(create(BarLayer))
+globals().update(create(FooLayer, BarLayer))
