2a69d34b2c6ff7c8ae748cbc069a405cc4cada00
Thomas Lotze <tl@gocept.com>
2013-08-16 09:33:01 +0200

    package boilerplate

---
+++ setup.py
@@ -0,0 +1,64 @@
+# Copyright (c) 2013 gocept gmbh & co. kg
+# See also LICENSE.txt
+
+# This should be only one line. If it must be multi-line, indent the second
+# line onwards to keep the PKG-INFO file format intact.
+"""Integration of zope.testrunner-style test layers into the py.test framework
+"""
+
+from setuptools import setup, find_packages
+import glob
+import os.path
+
+
+def project_path(*names):
+    return os.path.join(os.path.dirname(__file__), *names)
+
+
+setup(
+    name='gocept.pytestlayer',
+    version='0.1.dev0',
+
+    install_requires=[
+        'setuptools',
+    ],
+
+    extras_require={
+        'test': [
+        ],
+    },
+
+    entry_points={
+        'console_scripts': [
+            # 'binary-name = gocept.pytestlayer.module:function'
+        ],
+    },
+
+    author='gocept <mail@gocept.com>',
+    author_email='mail@gocept.com',
+    license='ZPL 2.1',
+    url='https://bitbucket.org/gocept/gocept.pytestlayer/',
+
+    keywords='',
+    classifiers="""\
+License :: OSI Approved :: Zope Public License
+Programming Language :: Python
+Programming Language :: Python :: 2
+Programming Language :: Python :: 2.6
+Programming Language :: Python :: 2.7
+Programming Language :: Python :: 2 :: Only
+"""[:-1].split('\n'),
+    description=__doc__.strip(),
+    long_description='\n\n'.join(open(project_path(name)).read() for name in (
+        'README.txt',
+        'HACKING.txt',
+        'CHANGES.txt',
+    )),
+
+    namespace_packages=['gocept'],
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    data_files=[('', glob.glob(project_path('*.txt')))],
+    zip_safe=False,
+)
