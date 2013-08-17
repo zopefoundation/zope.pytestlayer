0414942d7e252cf0fef187fa3de3f180a41a1390
Thomas Lotze <tl@gocept.com>
2013-08-17 14:24:33 +0200

    renamed ReST files to .rst

--- doc/conf.py
+++ doc/conf.py
@@ -2,6 +2,8 @@
 # See also LICENSE.txt
 
 _year_started = 2013
+source_suffix = '.rst'
+autosummary_generate = ['api.rst']
 
 import gocept.package.sphinxconf
 gocept.package.sphinxconf.set_defaults()
