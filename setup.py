7dc58a6cf314a835b75ec911c0ce54bf28d0a009
Michael Howitz <mh@gocept.com>
2013-08-28 11:48:56 +0200

    Polish classifiers

--- setup.py
+++ setup.py
@@ -47,12 +47,27 @@ setup(
 
     keywords='',
     classifiers="""\
+Development Status :: 4 - Beta
+Environment :: Console
+Framework :: Plone
+Framework :: Zope2
+Framework :: Zope3
+Intended Audience :: Developers
+License :: OSI Approved
 License :: OSI Approved :: Zope Public License
+Natural Language :: English
+Operating System :: OS Independent
 Programming Language :: Python
 Programming Language :: Python :: 2
 Programming Language :: Python :: 2.6
 Programming Language :: Python :: 2.7
 Programming Language :: Python :: 2 :: Only
+Programming Language :: Python :: Implementation
+Programming Language :: Python :: Implementation :: CPython
+Topic :: Software Development
+Topic :: Software Development :: Libraries
+Topic :: Software Development :: Libraries :: Python Modules
+Topic :: Software Development :: Testing
 """[:-1].split('\n'),
     description=__doc__.strip(),
     long_description='\n\n'.join(open(project_path(name)).read() for name in (
