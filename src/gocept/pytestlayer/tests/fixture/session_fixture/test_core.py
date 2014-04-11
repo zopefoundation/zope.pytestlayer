d9a76f07280528315d6e8439b5e47711f789dd15
Wolfgang Schnerring <wosc@wosc.de>
2014-04-11 14:08:49 +0200

    Write test for session fixtures

---
+++ src/gocept/pytestlayer/tests/fixture/session_fixture/test_core.py
@@ -0,0 +1,36 @@
+from gocept.pytestlayer.testing import log_to_terminal
+import unittest
+
+
+class FooLayer(object):
+
+    @classmethod
+    def setUp(cls):
+        cls.layer_foo = 'layer foo'
+
+    @classmethod
+    def tearDown(cls):
+        del cls.layer_foo
+
+    @classmethod
+    def testSetUp(cls):
+        log_to_terminal(cls.pytest_request, 'testSetUp foo')
+        cls.test_foo = 'test foo'
+
+    @classmethod
+    def testTearDown(cls):
+        log_to_terminal(cls.pytest_request, 'testTearDown foo')
+        del cls.test_foo
+
+
+def test_can_access_layer_via_fixture(foo_layer_session):
+    assert 'layer foo' == foo_layer_session.layer_foo
+
+
+class FooTest(unittest.TestCase):
+
+    layer = FooLayer
+
+    def test_accesses_fixture_with_generated_name_for_layer(self):
+        self.assertEqual('layer foo', self.layer.layer_foo)
+        self.assertEqual('test foo', self.layer.test_foo)
